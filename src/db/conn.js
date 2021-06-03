8c4d293702303e1b0d541579dacfb9cf74995f6d
jhautsav <utsavjha2020@gmail.com>
2021-06-03 20:20:04 +0530

    this is for another try to fix it

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect(' mongodb+srv://admin:Utsav@1998@cluster0.rfnpr.mongodb.net/gymdata',{
+mongoose.connect('mongodb+srv://admin:Utsav@1998@cluster0.rfnpr.mongodb.net/gymdata',{
     
     useNewUrlParser: true ,
     useUnifiedTopology: true,
