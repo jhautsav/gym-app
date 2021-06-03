190ae9aca313bbc5a5a01ff221389e8c6dd2f1f1
jhautsav <utsavjha2020@gmail.com>
2021-06-03 19:03:13 +0530

    this is for adding admin panel

--- src/app.js
+++ src/app.js
@@ -6,20 +6,21 @@ const port = process.env.PORT || 5000;
 const path = require('path');
 const hbs = require('hbs');
 const nodemailer = require('nodemailer');
-const multer=require('multer');
+
+/*this is for uploading image for admin panel */
+const multer = require('multer');
+/*this is for end uploading image for admin panel */
 
 /*require connection */
 require('./db/conn');
-
 /*require end of connection */
 
 /*require schema and models*/
 const login = require('./models/login');
 const maildetails = require('./models/email');
 const trainer = require('./models/trainerdetails');
- 
-
 /*require end of schema and models*/
+
 
 /*for using bcrupt function */
 const bcrypt = require('bcryptjs');
@@ -45,23 +46,203 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 /* end code for reading front end data */
 
-//for serving dynamic website using hbs
+/*for serving dynamic website using hbs */
 app.set('view engine', 'hbs');
 app.set('views', templatePath);
-//end of code serving dynamic websit using hbs
+/*end of code serving dynamic websit using hbs */
+
+
+//***********ROUTING**********************/
 app.get('/', (req, res) => {
     res.render('signup');
 })
+
 app.get('/signup', (req, res) => {
-
-
     res.render('signup');
 })
-app.get('/admin',(req,res)=>{
-  res.render('')
-})
+
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+//here fetch data from database
+const storeData = login.find({});
+app.get('/indexadmin', async (req, res) => {
+    const dataPrint = await storeData.exec((err, data) => {
+        if (err) { throw err; }
+        else {
+
+            res.render('indexadmin', { result: data });
+        }
+    })
+})
+app.get('/user', (req, res) => {
+    res.redirect('/indexadmin');
+})
+//this is for delting
+app.get('/delete/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const del = login.findByIdAndDelete(id);
+    const dataPrint = await del.exec((err) => {
+        if (err) { throw err; }
+        else {
+
+            res.redirect('/indexadmin');
+
+        }
+
+    })
+})
+app.get('/edit/:id', async (req, res) => {
+    const id = req.params.id;
+    const edit = login.findById(id);
+    const findData = await edit.exec((err, data) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.render('edit', { title: 'Edit your userdata', resultdata: data });
+
+        }
+
+    })
+
+
+})
+//for updating
+
+app.post('/update', async (req, res) => {
+
+    const update = login.findByIdAndUpdate(req.body.id, {
+        fName: req.body.fName,
+        lName: req.body.LName,
+        phone: req.body.phone,
+        email: req.body.email
+    })
+
+    const updateData = await update.exec((err, data) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.redirect('/indexadmin');
+
+        }
+
+    })
+})
+//herer for mail 
+const emaildataget = emaildetails.find({});
+app.get('/mail', async (req, res) => {
+
+
+    const emaildataPrint = await emaildataget.exec((err, data) => {
+        if (err) { throw err; }
+        else {
+
+            res.render('mail', { emailresult: data });
+
+        }
+
+    })
+})
+//     //maildeleting
+app.get('/delemail/:id', async (req, res) => {
+    const id = req.params.id;
+    const deletedata = emaildetails.findByIdAndDelete(id);
+    const deleteafterdata = await deletedata.exec((err) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.redirect('/mail')
+        }
+    })
+
+})
+
+//mail editing
+//first finding 
+app.get('/editmail/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const readdata = emaildetails.findById(id);
+
+    const savereaddata = await readdata.exec((err, data) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.render('editmail', { resultmail: data });
+        }
+    })
+
+})
+//update
+app.post('/emailupdate', async (req, res) => {
+    const updateemail = emaildetails.findByIdAndUpdate(req.body.id, {
+        username: req.body.username,
+        email: req.body.email,
+        subject: req.body.subject,
+        text: req.body.text
+    })
+    const updateemaildata = await updateemail.exec((err, data) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.redirect('/mail');
+
+        }
+    })
+})
+app.get('/dataupload', (req, res) => {
+    res.render('dataupload');
+})
+//this is for gettting name of file
+
+const storage = multer.diskStorage({
+    destination: "../public/uploads",
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+});
+
+//this is middleware for single file
+var upload = multer({
+    storage: storage
+}).single('file'); //'file' yaha pe filedname hai
+
+
+
+app.post('/imgupload', upload, (req, res) => {
+    const file = req.file.filename;
+    console.log(file);
+    const category = req.body.category;
+    const fullName = req.body.fullName;
+    const desc = req.body.desc;
+    const facebook = req.body.facebook;
+    const twitter = req.body.twitter;
+    const linkedin = req.body.twitter;
+    const trainerDetails = new trainer({
+        file: file,
+        category: category,
+        fullName: fullName,
+        desc: desc,
+        facebook: facebook,
+        twitter: twitter,
+        linkedin: linkedin
+    })
+
+    const finalTrainerDetails = trainerDetails.save((err, data) => {
+        if (err) { throw err; }
+        else {
+            res.redirect('indexadmin');
+        }
+
+
+    })
 })
 
 
@@ -163,7 +344,11 @@ app.post('/email', async (req, res) => {
                 console.log(error);
             }
             else {
-                res.render('index', { scuess: '', result: true, value1: 'success' });
+                trainerData.exec((err, data) => {
+                    res.status(201).render('index', { scuess: 'You have been successfully sent mail.', result: true, imagedata: data, value1: 'success' });
+
+                })
+
             }
         })
         const saveInsert = await saveData.save();
@@ -171,268 +356,11 @@ app.post('/email', async (req, res) => {
     catch (e) {
         res.status(401).send(e);
     }
-
-
-
-
 })
 app.get('/*', (req, res) => {
     res.render('404');
 })
 
-
- 
- 
-
- 
- 
- 
-const storeData=login.find({});
-
-
-//routing
-  app.get('/admin', async (req,res)=>
-    {
-       const dataPrint=  await storeData.exec((err,data)=>{
-             if(err) {throw err;}
-             else
-             {
-                 
-                res.render('indexadmin',{result:data});
-                
-             }
-             
-         }) 
-    
-    })
-    app.get('/dataupload',(req,res)=>{
-        res.render('dataupload');
-    })
-  //this is for gettting name of file
- 
- const storage=multer.diskStorage({
-     destination:"../public/uploads",
-     filename:(req,file,cb)=>{
-         cb(null,file.fieldname+"_"+Date.now()+path.extname(file.originalname));
-     }
- });
-
-//this is middleware for single file
- var upload=multer({
-    storage:storage
-}).single('file'); //'file' yaha pe filedname hai
-
-
-
- app.post('/imgupload',upload,(req,res)=>
-    {
-const file=req.file.filename;
-console.log(file);
-const category=req.body.category;
-const fullName=req.body.fullName;
-const desc=req.body.desc;
-const facebook=req.body.facebook;
-const twitter=req.body.twitter;
-const linkedin=req.body.twitter;
- const trainerDetails=new trainer ({
-     file:file,
-     category:category,
-     fullName:fullName,
-     desc:desc,
-     facebook:facebook,
-     twitter:twitter,
-     linkedin:linkedin
-})
- 
-const finalTrainerDetails=trainerDetails.save((err,data)=>{
-        if(err){ throw err;}
-        else{ 
-            res.send(finalTrainerDetails);
-        }
- 
-
-    })
-})
-  
-
-    //this is for delting
-     app.get('/delete/:id',async(req,res)=>{
-         const id=req.params.id;
-         
-          const del=login.findByIdAndDelete(id);
-          const dataPrint=  await del.exec((err)=>{
-            if(err) {throw err;}
-            else
-            {
-               
-               res.redirect('/indexadmin');
-               
-            }
-            
-        }) 
-     })
-     
-     //this is for editing 
-
-               //for finding
-     app.get('/edit/:id',async(req,res)=>{
-         const id=req.params.id;
-         const edit=login.findById(id);
-         const findData=  await edit.exec((err,data)=>{
-            if(err)
-             {
-                 throw err;
-            }
-            else
-            {
-                res.render('edit',{title:'Edit your userdata',resultdata:data});
-               
-            }
-            
-        }) 
-        
-
-     })
-               //for updating
-
-    app.post('/update',async(req,res)=>{
-      
-        const update=login.findByIdAndUpdate(req.body.id,{
-            fName:req.body.fName,
-            lName:req.body.LName ,
-            phone:req.body.phone,
-            email:req.body.email
- })
- console.log(req.body.id);
- const updateData=  await update.exec((err,data)=>{
-    if(err)
-     {
-         throw err;
-    }
-    else
-    {
-        res.redirect('/indexadmin');
-       
-    }
-    
-}) 
-
-    })
-   
- 
-    const emaildataget=emaildetails.find({});
-   
-   
-   
-    //mail printing
-    app.get('/user',(req,res)=>{
-        res.redirect('/');
-    })
-    app.get('/mail', async (req,res)=>
-    {
-       
-       const emaildataPrint=  await emaildataget.exec((err,data)=>{
-             if(err) {throw err;}
-             else
-             {
-                 
-                res.render('mail',{emailresult:data});
-                
-             }
-             
-         }) 
-    
-    })
-
-    //maildeleting
-    app.get('/delemail/:id',async(req,res)=>{
-        const id=req.params.id;
-        const deletedata= emaildetails.findByIdAndDelete(id);
-        const  deleteafterdata=await deletedata.exec((err)=>{
-            if(err)
-            {
-                throw err;
-            }
-            else{
-                res.redirect('/mail')
-            }
-        })
-        
-    })
-
-    //mail editing
-       //first finding 
-       app.get('/editmail/:id',async(req,res)=>{
-        const id=req.params.id;
-         
-        const readdata=emaildetails.findById(id);
-        
-        const  savereaddata=await readdata.exec((err,data)=>{
-            if(err)
-            {
-                throw err;
-            }
-            else{
-                res.render('editmail',{resultmail:data});
-            }
-        })
-
-       })
-       //update
-       app.post('/emailupdate',async(req,res)=>{
-           const updateemail=emaildetails.findByIdAndUpdate(req.body.id,{
-               username:req.body.username,
-               email:req.body.email,
-               subject:req.body.subject,
-               text:req.body.text
-           })
-           const updateemaildata=await updateemail.exec((err,data)=>{
-            if(err)
-            {
-                throw err;
-           }
-           else
-           {
-               res.redirect('/mail');
-              
-           }
-           })
-       })
-
-/*
-
-                /////////this is for admin panel/////////////////
-**************************************************************************************
- 
-
-  
-
-      
-  
-       
-
-     
-     
-    
-    
-
-//listing
-app.listen(port,()=>{
-    console.log(`yes connecting at ${port}`);
-})
-
-
-
-
-
-*/
-
-
-
-
-
-
- 
 app.listen(port, () => {
     console.log(`connecte at server ${port}`);
 })
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
-// // mongoose.connect(' mongodb+srv://admin:Utsav@1998@cluster0.rfnpr.mongodb.net/gymdata',{
-    mongoose.connect('mongodb://localhost:27017/gymdata',
-    { 
+mongoose.connect(' mongodb+srv://admin:Utsav@1998@cluster0.rfnpr.mongodb.net/gymdata',{
+    // mongoose.connect('mongodb://localhost:27017/gymdata',
+    // { 
     useNewUrlParser: true ,
     useUnifiedTopology: true,
     useCreateIndex:true,
